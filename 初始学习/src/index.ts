ea1cbaac2622f627428b83e8fe604221542bd9ab
PhilRandWu <3378814052@qq.com>
2022-03-03 09:43:32 +0800

    初始定义

--- 初始学习/src/index.ts
+++ 初始学习/src/index.ts
@@ -1,13 +1,20 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
- * @Date: 2022-02-17 10:52:18
- * @LastEditTime: 2022-02-25 16:10:03
+ * @Date: 2022-03-03 09:38:41
+ * @LastEditTime: 2022-03-03 09:43:06
  * @LastEditors: PhilRandWu
  */
-
-import { createCard, printCard } from "./function";
-
-const pokers = createCard();
-printCard(pokers);
+class User {
+  readonly id: number;
+  name: string;
+  age: number;
+  gender: "男" | "女" = "男";
+  pid?: string;
+  constructor(name: string, age: number) {
+    this.id = Math.random();
+    this.name = name;
+    this.age = age;
+  }
+}
