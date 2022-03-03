71b9c59d98867dda2fdbc1ef0d4858585e6858f8
PhilRandWu <3378814052@qq.com>
2022-03-03 10:19:37 +0800

    访问属性

--- 初始学习/src/index.ts
+++ 初始学习/src/index.ts
@@ -3,18 +3,33 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-03-03 09:38:41
- * @LastEditTime: 2022-03-03 09:43:06
+ * @LastEditTime: 2022-03-03 10:18:33
  * @LastEditors: PhilRandWu
  */
 class User {
   readonly id: number;
-  name: string;
-  age: number;
   gender: "男" | "女" = "男";
   pid?: string;
-  constructor(name: string, age: number) {
+
+  private ReleaseNumber: number = 0;
+  private CeilReleaseNumber: number = 3;
+  constructor(public name: string, public age: number) {
     this.id = Math.random();
-    this.name = name;
-    this.age = age;
+  }
+  publish(title: string) {
+    if(this.ReleaseNumber < this.CeilReleaseNumber) {
+        console.log('发布一篇文章' + title);
+        this.ReleaseNumber ++;
+    } else {
+        console.log('您今日发布文章已达上限');
+    }
   }
 }
+
+
+const user = new User('小米',15);
+console.log(user);
+user.publish('竞争力');
+user.publish('竞争力');
+user.publish('竞争力');
+user.publish('竞争力');
