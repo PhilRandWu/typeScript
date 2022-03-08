5c38057da3b4ad75c1f3314e1b17d1a5ead76c0f
PhilRandWu <3378814052@qq.com>
2022-03-08 11:06:59 +0800

    类化

--- 初始学习/src/index.ts
+++ 初始学习/src/index.ts
@@ -3,11 +3,24 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-02-17 10:52:18
- * @LastEditTime: 2022-02-25 16:10:03
+ * @LastEditTime: 2022-03-08 11:05:57
  * @LastEditors: PhilRandWu
  */
 
-import { createCard, printCard } from "./function";
+import { Poker } from "./poker";
 
-const pokers = createCard();
-printCard(pokers);
+
+const pokers = new Poker();
+pokers.shuffle();
+pokers.printCard();
+
+
+
+console.log('发牌');
+const playerArr = pokers.publish();
+console.log(playerArr);
+playerArr[0].printCard();
+playerArr[1].printCard();
+playerArr[2].printCard();
+playerArr[3].printCard();
+playerArr[3].shuffle();
