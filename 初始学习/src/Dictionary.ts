e610d58af6458fed9afdaec311278715ccf85d54
PhilRandWu <3378814052@qq.com>
2022-03-10 10:15:19 +0800

    字典泛型

--- 初始学习/src/Dictionary.ts
+++ 初始学习/src/Dictionary.ts
@@ -3,14 +3,47 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-03-10 09:52:17
- * @LastEditTime: 2022-03-10 09:53:27
+ * @LastEditTime: 2022-03-10 10:13:51
  * @LastEditors: PhilRandWu
  */
-class Dictionary<k, v> {
+export type Callback<t,u> = (key:t, value:u) => void;
+
+export class Dictionary<k, v> {
   private _key: k[] = [];
   private _value: v[] = [];
 
   set(key: k, value: v) {
-      
+    const index = this._key.indexOf(key);
+    if (index !== -1) {
+      this._value[index] = value;
+    } else {
+      this._key.push(key);
+      this._value.push(value);
+    }
+  }
+
+  get size():number {
+      return this._key.length;
+  }
+
+  has(key: k) {
+    return this._key.includes(key);
+  }
+
+  delete(key:k) {
+    const index = this._key.indexOf(key);
+    if(index === -1) {
+        return;
+    } else {
+        this._key.splice(index, 1);
+        this._value.splice(index, 1);
+    }
+  }
+
+  forEach(callback:Callback<k,v>) {
+    this._key.forEach((k,i) => {
+        const v = this._value[i];
+        callback(k,v);
+    })
   }
 }
--- 初始学习/src/index.ts
+++ 初始学习/src/index.ts
@@ -3,7 +3,21 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-02-17 10:52:18
- * @LastEditTime: 2022-03-10 09:50:35
+ * @LastEditTime: 2022-03-10 10:14:36
  * @LastEditors: PhilRandWu
  */
 
+import { Dictionary } from "./Dictionary";
+
+const dictionary = new Dictionary();
+dictionary.set('name', 'John');
+dictionary.set('age', 12);
+dictionary.forEach((item, value) => {
+    console.log(item,value)
+})
+console.log(dictionary);
+console.log(dictionary.size)
+console.log(dictionary.has('name'));
+dictionary.delete('name');
+console.log(dictionary);
+console.log(dictionary.size);
