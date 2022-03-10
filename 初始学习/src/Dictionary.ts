c4ef44e962e0386d4a4c52f2fbf2555a2e20abf9
PhilRandWu <3378814052@qq.com>
2022-03-10 09:54:01 +0800

    文件目录

---
+++ 初始学习/src/Dictionary.ts
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Author: PhilRandWu
+ * @Github: https://github/PhilRandWu
+ * @Date: 2022-03-10 09:52:17
+ * @LastEditTime: 2022-03-10 09:53:27
+ * @LastEditors: PhilRandWu
+ */
+class Dictionary<k, v> {
+  private _key: k[] = [];
+  private _value: v[] = [];
+
+  set(key: k, value: v) {
+      
+  }
+}
